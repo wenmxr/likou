c28ce5e74ef566c6b253036a8d0519d86b51596a
qinwen <qinwen@dian.so>
2021-09-10 10:15:27 +0800

    feat: add first

---
+++ src/main/java/com/wenmxr/algorithm/TwoSum.java
@@ -0,0 +1,50 @@
+package com.wenmxr.algorithm;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 1.两数之和
+ *
+ * @Author qinwen
+ * @Date 2021/8/25 12:40 上午
+ */
+public class TwoSum {
+
+    public static void main(String[] args) {
+        long now = System.currentTimeMillis();
+        System.out.println(Arrays.toString(twoSum1(new int[]{2, 7, 11, 15}, 9)));
+        System.out.println(System.currentTimeMillis() - now);
+
+
+    }
+
+    /**
+     * double for
+     */
+    public static int[] twoSum1(int[] nums, int target) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[0];
+    }
+
+    /**
+     * hash table
+     */
+    public static int[] twoSum2(int[] nums, int target) {
+        Map<Integer, Integer> hashTable = new HashMap<>(nums.length);
+        for (int i = 0; i < nums.length; i++) {
+            if (hashTable.containsKey(target - nums[i])) {
+                return new int[]{hashTable.get(target - nums[i]), i};
+            }
+            hashTable.put(target - nums[i], i);
+        }
+        return new int[0];
+    }
+}
---
+++ src/main/java/com/wenmxr/algorithm/ValidParentheses.java
@@ -0,0 +1,67 @@
+package com.wenmxr.algorithm;
+
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+
+/**
+ * 20.有效的括号
+ *
+ * @Author qinwen
+ * @Date 2021/8/26 8:47 上午
+ */
+public class ValidParentheses {
+    public static void main(String[] args) {
+        System.out.println(isValid1("([{}])"));
+        System.out.println(isValid2("([{}])"));
+        System.out.println(isValid3("(("));
+    }
+
+    public static boolean isValid1(String s) {
+        if (s.length() % 2 != 0) {
+            return false;
+        }
+
+        while (s.contains("()") || s.contains("[]") || s.contains("{}")) {
+            s = s.replace("()", "").replace("[]", "").replace("{}", "");
+        }
+        return s.isEmpty();
+    }
+
+    public static boolean isValid2(String s) {
+        int length = s.length();
+        if (length % 2 != 0) {
+            return false;
+        }
+        for (int i = 0; i < length / 2; i++) {
+            s = s.replace("()", "").replace("[]", "").replace("{}", "");
+        }
+        return s.isEmpty();
+    }
+
+
+    public static boolean isValid3(String s) {
+        int length = s.length();
+        if (length % 2 != 0) {
+            return false;
+        }
+        HashMap<Character, Character> hashTable = new HashMap<>(3);
+        hashTable.put(')', '(');
+        hashTable.put(']', '[');
+        hashTable.put('}', '{');
+
+        Deque<Character> stack = new LinkedList<>();
+
+        for (int i = 0; i < length; i++) {
+            char c = s.charAt(i);
+            if (hashTable.containsKey(c)) {
+                if (stack.isEmpty() || !hashTable.get(c).equals(stack.pop())) {
+                    return false;
+                }
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack.isEmpty();
+    }
+}
